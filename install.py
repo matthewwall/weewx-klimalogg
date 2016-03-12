b905c1c850c1931b1a7d222be1bfcd482a792da6
Matthew Wall <mwall@picorino.local>
2016-03-12 12:43:16 -0500

    update version in supporting documents

--- install.py
+++ install.py
@@ -10,7 +10,7 @@ def loader():
 class KlimaLoggInstaller(ExtensionInstaller):
     def __init__(self):
         super(KlimaLoggInstaller, self).__init__(
-            version="1.1.5",
+            version="1.1.7",
             name='klimalogg',
             description='Collect and display KlimaLogg Pro sensor data with kl skin',
             author="Luc Heijst",
